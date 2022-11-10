37956a641fb7011800ee495159c5669087bc7727
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2022-11-10 22:16:15 +0530

    Update removingevennum.py

--- removingevennum.py
+++ removingevennum.py
@@ -1,10 +1,10 @@
-list=[]
-n=int(input("enter the size of the list"))
-for i in range (0,n):
-    b=int(input("enter integer numbers"))
-    list.append(b)
-print("the orginal list is",list)
-for elem in list:
-    if(elem%2==0):
-        list.remove(elem)
-print("the new list is",list)
+
+list1=[]
+odd=[]
+n=int(input("enter list size : "))
+for i in range(n):
+    a=(int(input("Enter a number  :")))
+    list1.append(a)
+print("the orginal list is",list1)
+odd=[each for each in list1 if each%2!=0]
+print("the new list is",odd)
