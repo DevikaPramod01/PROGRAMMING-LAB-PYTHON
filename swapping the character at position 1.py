e869ab55d10da063375ee8ffef5102163cd85a77
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2022-11-10 22:21:29 +0530

    Create swapping the character at position 1.py

---
+++ swapping the character at position 1.py
@@ -0,0 +1,8 @@
+string1=input("enter string 1 \t")
+string2=input("enter string 2\t")
+char1=string1[0]
+char2=string2[0]
+var=char2+string1[1:]
+var1=char1+string2[1:]
+a=var + ' '+ var1
+print("the new text is",a)
