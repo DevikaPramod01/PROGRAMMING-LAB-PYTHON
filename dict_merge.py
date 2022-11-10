f96fba3c5eebe25e8aa604f5efc4a801dd4b0d73
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2022-11-10 22:17:18 +0530

    Update dict_merge.py

--- dict_merge.py
+++ dict_merge.py
@@ -1,19 +1,26 @@
+
+
 dict1={}
-n=int(input("enter the num of items"))
+n=int(input("Enter the no.of items in the dictionary:"))
 for i in range(n):
-    key=input("enter keys")
-    value=input("enter value")
+    key=input("Enter key: ")
+    value=input("Enter value: ")
     dict1[key]=value
-print("first dictionary is",dict1)
+print(dict1)
 
 dict2={}
-n1=int(input("enter the num of items"))
-for i in range(n1):
-    key=input("enter keys")
-    value=input("enter value")
+m=int(input("Enter the no.of items in the dictionary:"))
+for i in range(m):
+    key=input("Enter key: ")
+    value=input("Enter value: ")
     dict2[key]=value
-print("second dictionary is",dict2)
+print(dict2)
+#dict1.update(dict2)
+#print(dict1)
+dict3={}
+dict3={**dict1,**dict2}
+for x,y in dict3.items():
+    if x in dict1 and x in dict2:
+        dict3[x]=[y,dict1[x]]
 
-print("after merging dict1 with dict 2")
-dict1.update(dict2)
-print(dict1)
+print("the merged dictionary is\n",dict3)
