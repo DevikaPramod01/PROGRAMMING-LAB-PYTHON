14430bbb2c67e92090342dc22453c84a2f0156e0
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2022-12-08 17:33:09 +0530

    Create add -ing & -ly function.py

---
+++ add -ing & -ly function.py
@@ -0,0 +1,8 @@
+def string(a):
+    if a[-1]=="g" and a[-2]=="n" and a[-3]=="i":
+        a.append("ly")
+    else:
+        a.append("ing")
+    print(''.join(a))
+j=list(input("enter the word"))
+string(j)
