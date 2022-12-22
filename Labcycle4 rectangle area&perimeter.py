59381f1aab8e190a9cbd049ecc964e23a7cf9f0e
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2022-12-22 10:52:58 +0530

    Create Labcycle4 rectangle area&perimeter.py

---
+++ Labcycle4 rectangle area&perimeter.py
@@ -0,0 +1,31 @@
+
+class rectangle():
+    def __init__(self,l,b):
+        self.length=l
+        self.breadth=b
+    def area(self):
+        return self.length * self.breadth
+    def perimeter(self):
+        return 2*(self.length+ self.breadth)
+
+l1=int(input("enter length 1 "))
+b1=int(input("enter breadth 1 "))
+
+l2=int(input("enter length 2 "))
+b2=int(input("enter breadth 2 "))
+
+rect1=rectangle(l1,b1)
+rect2=rectangle(l2,b2)
+
+
+print("area of rectangle 1:",rect1.area())
+print("perimeter of rectangle 1:",rect1.perimeter())
+
+print("area of rectangle 2:",rect2.area())
+print("perimeter of rectangle 2:",rect2.perimeter())
+
+               
+        
+            
+	
+
