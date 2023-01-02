3e1d683f6f9e8ae16c8374e828dcbe95d2662879
DevikaPramod01 <115997408+DevikaPramod01@users.noreply.github.com>
2023-01-02 15:03:01 +0530

    Update labcycle4 rectangle area&perimeter.py

--- labcycle4 rectangle area&perimeter.py
+++ labcycle4 rectangle area&perimeter.py
@@ -10,19 +10,26 @@ class rectangle():
 
 l1=int(input("enter length 1 "))
 b1=int(input("enter breadth 1 "))
-
 l2=int(input("enter length 2 "))
 b2=int(input("enter breadth 2 "))
 
 rect1=rectangle(l1,b1)
 rect2=rectangle(l2,b2)
 
-
 print("area of rectangle 1:",rect1.area())
 print("perimeter of rectangle 1:",rect1.perimeter())
-
 print("area of rectangle 2:",rect2.area())
 print("perimeter of rectangle 2:",rect2.perimeter())
+print("\n comparing rectangles....")
+
+if rect1.area()==rect2.area():
+    print("\n area of rectangles are equal")
+else:
+    print("\n area of rectangles are different")
+if rect1.perimeter()==rect2.perimeter():
+    print("\n perimeter of rectangles are equal")
+else:
+    print("\n perimeter of rectangles are different")
 
                
         
